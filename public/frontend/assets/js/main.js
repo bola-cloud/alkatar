0bd0d2ec45b624504df32ee0b1397dcabf1460b6
muscatapps <muscatapps@gmail.com>
2024-08-10 11:02:10 +0000

    chore: Remove commented preloader code and update hero section height in CSS

--- public/frontend/assets/js/main.js
+++ public/frontend/assets/js/main.js
@@ -4,10 +4,10 @@
     /*----------------------------
      Preloader
     ------------------------------ */
-    $(window).on("load", function () {
-        $("#status").fadeOut();
-        $("#preloader").delay(350).fadeOut("slow");
-    });
+    // $(window).on("load", function () {
+    //     $("#status").fadeOut();
+    //     $("#preloader").delay(350).fadeOut("slow");
+    // });
 
     /*-------------------------------------------
   Sticky Header
